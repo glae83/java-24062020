1cccd3fc8a9d15fcfd60c6c7c87302c11e707c4f
glae83 <glae83@gmail.com>
2020-07-03 12:26:13 +0400

    lesson 6

--- src/lesson6/Client.java
+++ src/lesson6/Client.java
@@ -1,4 +1,63 @@
 package lesson6;
 
+import java.io.*;
+import java.net.*;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        clientMain clientMain = new clientMain();
+        System.out.println("Клиент подключился");
+
+        new Thread() {
+            public void run() {
+                try {
+                    clientMain.readMessage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                try {
+                    clientMain.sendMessage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
+
+class clientMain {
+    Socket socket = null;
+    BufferedReader in = null;
+    PrintWriter out = null;
+    BufferedReader console = null;
+    String userMessage, serverMessage;
+
+    public clientMain() throws IOException {
+        socket = new Socket("localhost",8189);
+        in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(),true);
+        console = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    void sendMessage() throws IOException {
+        while (true) {
+            if ((userMessage = console.readLine()) != null) {
+                out.println(userMessage);
+                if (userMessage.equalsIgnoreCase("/end")) break;
+            }
+        }
+    }
+
+    void readMessage() throws IOException {
+        while (true) {
+            if ((serverMessage = in.readLine()) != null){
+                System.out.println(serverMessage);
+            }
+        }
+    }
 }
--- src/lesson6/Server.java
+++ src/lesson6/Server.java
@@ -1,4 +1,91 @@
 package lesson6;
 
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        serverMain serv = new serverMain();
+        serv.start();
+        serv.catchClient();
+        new Thread() {
+            public void run() {
+                while (true) {
+                    String txt = null;
+                    try {
+                        txt = serv.in.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (txt != null) {
+                        try {
+                            serv.sendMessage(txt);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                try {
+                    serv.writeToConsole();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
+
+class serverMain {
+    BufferedReader in = null;
+    PrintWriter out = null;
+    ServerSocket serverSocket = null;
+    Socket socket = null;
+    BufferedReader console = null;
+
+    void start() {
+        try {
+            serverSocket = new ServerSocket(8189);
+        } catch (IOException e) {
+            System.exit(1);
+        }
+        System.out.print("Сервер запущен");
+    }
+
+    void catchClient() throws IOException {
+        try {
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+        } catch (IOException e) {
+            System.exit(1);
+        }
+
+        in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(),true);
+    }
+
+    void sendMessage(String text) throws IOException {
+        if (text.equalsIgnoreCase("/end")) close();
+        out.println(text);
+        System.out.println(text);
+    }
+
+    void close() throws IOException {
+        out.close();
+        in.close();
+        socket.close();
+        serverSocket.close();
+    }
+
+    void writeToConsole() throws IOException {
+        while (true) {
+            console = new BufferedReader(new InputStreamReader(System.in));
+            String str = console.readLine();
+            sendMessage(str);
+        }
+    }
 }
